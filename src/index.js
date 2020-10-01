6fd6f8e7d7a880c23b60bfd7198a814318cf266f
sergeytonchenko <randick56@gmail.com>
2020-10-01 20:18:53 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel';
 
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
+    $('.slide-one').owlCarousel({
       loop: true,
       nav: true,
       navText: ["",""],
       items: 3,
+      margin: 10, 
+      autoplay: false,
+      autoplayTimeout: 2000            
+    });
+
+    $('.slide-two').owlCarousel({
+      loop: true,
+      nav: true,
+      navText: ["",""],
+      items: 1,
       margin: 10, 
       autoplay: false,
       autoplayTimeout: 2000            
